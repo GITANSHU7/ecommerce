76bedd6289ece93fcd03075731f1f4e5fffe6192
GITANSHU7 <gitanshugautam7@gmail.com>
2020-12-21 22:57:45 +0530

    t

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useEffect, useState} from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
@@ -12,8 +12,29 @@ import "react-toastify/dist/ReactToastify.css";
 import RegisterComplete from "./pages/auth/RegisterComplete";
 import {auth}from './firebase';
 import {useDispatch} from 'react-redux'
-
 const App = () => {
+  const dispatch = useDispatch();
+
+  // to check firebase auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+        console.log("user", user);
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+    // cleanup
+    return () => unsubscribe();
+  }, []);
+
+
   return (
     <>
     {logo()}
