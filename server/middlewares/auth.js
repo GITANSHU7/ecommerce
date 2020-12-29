33ea236ed17cff1a1fdc20bcf3f77ec35ae4e5a0
GITANSHU7 <gitanshugautam7@gmail.com>
2020-12-29 12:32:09 +0530

    4

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -4,7 +4,8 @@ const admin = require("../firebase");
 exports.authCheck = (req, res, next) => {
   //console.log(req.headers); // token
   try{
-
+    const firebaseUser = await admin.auth().verifyIdToken(req.headers.authtoken)
+    console.log('Firebase user in authcheck', firebaseUser )
   }catch (err){
     console.log(err);
     res.status(401).json({
