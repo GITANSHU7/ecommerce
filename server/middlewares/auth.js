c816cf5e5abf2ff005c544a968d50950479dcb31
GITANSHU7 <gitanshugautam7@gmail.com>
2020-12-29 12:20:45 +0530

    1

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,12 +1,14 @@
-
-
-
 const admin = require("../firebase");
-
-
 
 // to check whether the admin is valid
 exports.authCheck = (req, res, next) => {
-  console.log(req.headers); // token
-  next();
+  //console.log(req.headers); // token
+  try{
+
+  }catch (err){
+    res.status(401).json({
+      err:"Invalid or expired token"
+    })
+  }
+  
 };
