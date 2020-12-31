a5f462488eda6178cf3677cae664b062b4dc39eb
GITANSHU7 <gitanshugautam7@gmail.com>
2020-12-31 09:17:35 +0530

    11

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,8 +6,14 @@ const router = express.Router();
 const { authCheck } = require("../middlewares/auth");
 
 // controller
-const { createOrUpdateUser } = require("../controllers/auth");
+const { createOrUpdateUser , currentUser } = require("../controllers/auth");
 
 router.post("/create-or-update-user", authCheck, createOrUpdateUser);
+
+//after reload in page we lost the data like name, role & we donot have to query data base to update the user thats use less
+// so we create  a new end point that will gives us currently logged in user 
+
+router.post("/current-user", authCheck, currentUser);
+
 
 module.exports = router;
