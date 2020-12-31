caf95cbf949e43e80f9cce072541192f5193a6b1
GITANSHU7 <gitanshugautam7@gmail.com>
2020-12-31 09:28:50 +0530

    74

--- client/src/App.js
+++ client/src/App.js
@@ -39,7 +39,7 @@ const App = () => {
             }, 
           });
         }))
-        .catch();
+        .catch(err => console.log(err));
       }
     });
     // cleanup
--- client/src/pages/auth/RegisterComplete.js
+++ client/src/pages/auth/RegisterComplete.js
@@ -65,7 +65,7 @@ const RegisterComplete = ({ history }) => {
             }, 
           });
         }))
-        .catch();
+        .catch(err => console.log(err));
         // redirect
 
         history.push("/");
