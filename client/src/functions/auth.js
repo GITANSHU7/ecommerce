f0e640da9336fd0c99ae4a2675ada4bdfc5a75bc
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-01 22:38:14 +0530

    123

--- client/src/functions/auth.js
+++ client/src/functions/auth.js
@@ -21,3 +21,14 @@ import axios from "axios";
     }
       )
   }
+
+   
+ export const currentAdmin = async (authtoken) => {
+  return await axios.post(
+    `${process.env.REACT_APP_API}/current-admin ` , {},{
+    headers: {
+      authtoken,
+    }
+  }
+    )
+}
