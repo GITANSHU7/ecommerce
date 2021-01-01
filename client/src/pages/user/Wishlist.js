b1ff8634b1f8102eedafcdd0f58a1c64ac269002
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-01 20:21:09 +0530

    122

--- client/src/pages/user/Wishlist.js
+++ client/src/pages/user/Wishlist.js
@@ -1,7 +1,7 @@
 import React from "react";
 import UserNav from "../../components/nav/UserNav";
 
-const History = () => (
+const Wishlist = () => (
   <div className="container-fluid">
     <div className="row">
       <div className="col-md-2">
