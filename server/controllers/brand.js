878113d6e58298f80c49068015e7d231b9894326
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-03 14:21:15 +0530

    1000

--- server/controllers/brand.js
+++ server/controllers/brand.js
@@ -1,4 +1,4 @@
-const brand = require("../models/brand");
+const Brand = require("../models/brand");
 const slugify = require("slugify");
 
 exports.create = async (req, res) => {
@@ -9,7 +9,7 @@ exports.create = async (req, res) => {
     ////Sort the result by name:
   //var sort = { name: 1 };
   //dbo.collection("customers").find().sort(sort).toArray(function(err, result)
-    res.json(await new brand({ name, slug: slugify(name) }).save());
+    res.json(await new Brand({ name, slug: slugify(name) }).save());
   } catch (err) {
     // console.log(err);
     res.status(400).send("Create brand failed");
@@ -17,17 +17,17 @@ exports.create = async (req, res) => {
 };
 
 exports.list = async (req, res) =>
-  res.json(await brand.find({}).sort({ createdAt: -1 }).exec());
+  res.json(await Brand.find({}).sort({ createdAt: -1 }).exec());
 
 exports.read = async (req, res) => {
-  let brand = await brand.findOne({ slug: req.params.slug }).exec();
+  let Brand = await Brand.findOne({ slug: req.params.slug }).exec();
   res.json(brand);
 };
 
 exports.update = async (req, res) => {
   const { name } = req.body;
   try {
-    const updated = await brand.findOneAndUpdate(
+    const updated = await Brand.findOneAndUpdate(
       { slug: req.params.slug },
       { name, slug: slugify(name) },
       { new: true }
@@ -40,7 +40,7 @@ exports.update = async (req, res) => {
 
 exports.remove = async (req, res) => {
   try {
-    const deleted = await brand.findOneAndDelete({ slug: req.params.slug });
+    const deleted = await Brand.findOneAndDelete({ slug: req.params.slug });
     res.json(deleted);
   } catch (err) {
     res.status(400).send("Create delete failed");
