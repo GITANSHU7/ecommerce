11f81cc562f783b134d63c177b1d7627d32313da
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-03 11:29:02 +0530

    11001

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -69,8 +69,12 @@ const Header = () => {
           title={user.email && user.email}
           className="float-right"
         >
-          <Item key="setting:1">Option 1</Item>
-          <Item key="setting:2">Option 2</Item>
+          {user && user.role === "subscriber" && (
+            <Item><Link to = "/user/history">Dashboard</Link></Item>
+          )}
+           {user && user.role === "admin" && (
+            <Item><Link to = "/admin/dashboard">Dashboard</Link></Item>
+          )}
           <Item icon={  <GoSignOut  style={{marginRight:'7px', marginBottom:'-2px'}}/>} onClick={logout}>
             Logout
           </Item>
