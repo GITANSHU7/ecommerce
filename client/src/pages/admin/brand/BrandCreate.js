891a129f0a696c2c8bd1816582c0a084783a049f
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-06 09:21:29 +0530

    createbrand

---
+++ client/src/pages/admin/brand/BrandCreate.js
@@ -0,0 +1,17 @@
+import React from "react";
+import AdminNav from "../../../components/nav/AdminNav";
+
+const BrandCreate = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <AdminNav />
+        </div>
+        <div className="col">Brand create page</div>
+      </div>
+    </div>
+  );
+};
+
+export default BrandCreate;
