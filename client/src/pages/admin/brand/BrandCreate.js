26fa17091c8d1c17ea73e5962766f1b98993990d
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-07 22:09:29 +0530

    brand

--- client/src/pages/admin/brand/BrandCreate.js
+++ client/src/pages/admin/brand/BrandCreate.js
@@ -82,7 +82,7 @@ const handleRemove = async(slug) => {
           ) : (
             <h4>Create brand</h4>
           )}
-          {BrandForm()}
+          <BrandForm />
           <hr />
          <button className="btn btn-outline-info"> {brands.length}</button>
           <br />
