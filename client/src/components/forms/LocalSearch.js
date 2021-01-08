bee513de9cb9345677be22114331ff3f40129045
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-08 22:46:01 +0530

    001

--- client/src/components/forms/LocalSearch.js
+++ client/src/components/forms/LocalSearch.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const LocalSearch = () => {
-
+ return (
+     <div className="container pt-4 pb-4">
+  <input type= "search" 
+         placeholder="filter"
+         value={keyword}
+            onChange={handleSearchChange}
+            className="form-control mb-4"
+                />
+     </div>
+ )
 }
 
 export default LocalSearch;
