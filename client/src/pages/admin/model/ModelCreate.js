799e837784d51ada1564980b25f94f043d999002
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-09 09:02:35 +0530

    Model create

--- client/src/pages/admin/model/ModelCreate.js
+++ client/src/pages/admin/model/ModelCreate.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import AdminNav from '../../../components/nav/AdminNav';
 
+
+ 
 const ModelCreate = () => {
     return(
-    <div>
-    Model Page
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col-md-2">
+                <AdminNav />
+            </div>
+
+            <div className="col-md-10">
+                Model create
+            </div>
+        </div>
+    
 
     </div>
     )
