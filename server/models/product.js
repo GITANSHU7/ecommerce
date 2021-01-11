8178028e9b7e9fe7f3bc77e7cde992ebcda77b00
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-11 09:37:17 +0530

    upgradation

--- server/models/product.js
+++ server/models/product.js
@@ -50,10 +50,7 @@ const productSchema = new mongoose.Schema(
       type: String,
       enum: ["Yes", "No"],
     },
-    color: {
-      type: String,
-      enum: ["Black", "Brown", "Silver", "White", "Blue"],
-    },
+    
     transmission : {
         type: String,
         enum : ["Automatic" , "Manual"],
@@ -64,9 +61,33 @@ const productSchema = new mongoose.Schema(
      },
 
 
-    brand: {
+
+
+    year: {
       type: String,
-      enum: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS"],
+      enum: [
+          "2000",
+          "2001",
+          "2002",
+          "2003",
+          "2004",
+          "2005",
+          "2006",
+          "2007",
+          "2008",
+          "2009",
+          "2010",
+          "2011",
+          "2012",
+          "2013",
+          "2014",
+          "2015",
+          "2016",
+          "2017",
+          "2018",
+          "2019",
+          "2020",
+    ],
     },
     // ratings: [
     //   {
