c645436478c509bb926b32316530cb4c420060d3
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-18 22:46:20 +0530

    COMMIT

--- server/routes/product.js
+++ server/routes/product.js
@@ -5,11 +5,14 @@ const router = express.Router();
 const { authCheck, adminCheck } = require("../middlewares/auth");
 
 // controller
-const { create , listAll , remove} = require("../controllers/product");
+const { create , listAll , remove , read} = require("../controllers/product");
 
 // routes
 router.post("/product/product-create", authCheck, adminCheck, create);
 router.get("/products/:count",  listAll );
 router.delete("/product/:slug",  authCheck , adminCheck , remove);
+router.get("/products/:count",  listAll );
+router.get("/product/:slug" , read);
+
 
 module.exports = router;
