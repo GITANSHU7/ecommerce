abe0ce781f998ee1e28c0bd0431fa4cbd339e9a3
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-22 16:18:51 +0530

    updation 2

--- client/src/pages/admin/product/ProductCreate.js
+++ client/src/pages/admin/product/ProductCreate.js
@@ -23,12 +23,13 @@ const initialState = {
     fuels: ["Petrol" , "Diesel" ,"CNG" , "Electric"],
     fuel: "",
     quantity: "",
+    years: [  "2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020"],
+    year: "",
     images: []
 }
 
 const year = [  "2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020"]
 
-const [options] = useState(year);
 
 const ProductCreate = () => {
   const [values, setValues] = useState(initialState);
@@ -105,7 +106,6 @@ const ProductCreate = () => {
             handleBrandChange={handleBrandChange}
             modelOptions={modelOptions}
             showmodel={showModel}
-            options = {options}
           />
         </div>
       </div>
