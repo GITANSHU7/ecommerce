097bb4cef549d9ca50d3b391f089cced2d6ebb7d
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-23 21:22:34 +0530

    button

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -103,7 +103,7 @@ const FileUpload = ({ values, setValues, setLoading }) => {
           ))}
       </div>
       <div className="row">
-        <label className="btn btn-primary">
+        <label className="btn aqua-gradient">
           Choose File
           <input
             type="file"
