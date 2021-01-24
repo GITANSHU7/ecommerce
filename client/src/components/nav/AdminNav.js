36707708169aff84e1639472827228ce85bbbf29
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-24 20:44:43 +0530

    button

--- client/src/components/nav/AdminNav.js
+++ client/src/components/nav/AdminNav.js
@@ -1,59 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from 'reactstrap';
 
 const AdminNav = () => (
   <nav>
     <ul className="nav flex-column">
       <li className="nav-item">
         <Link to="/admin/dashboard" className="nav-link">
-          Dashboard
+        <Button color="success"> Dashboard </Button>
         </Link>
       </li>
 
       <li className="nav-item">
         <Link to="/admin/product/product-create" className="nav-link">
-          Product
+        <Button color="success"> Product</Button>
         </Link>
       </li>
 
       <li className="nav-item">
         <Link to="/admin/products" className="nav-link">
-          Products
+        <Button color="success"> Products</Button>
         </Link>
       </li>
 
       <li className="nav-item">
         <Link to="/admin/brand" className="nav-link">
-          Brand
+        <Button color="success"> Brand</Button>
         </Link>
       </li>
       <li className="nav-item">
         <Link to="/admin/model" className="nav-link">
-          Model
+        <Button color="success">Model</Button>
         </Link>
       </li>
 
 
       <li className="nav-item">
         <Link to="/admin/sub" className="nav-link">
-          Sub Category
+        <Button color="success"> Sub Category</Button>
         </Link>
       </li>
 
       <li className="nav-item">
         <Link to="/admin/coupon" className="nav-link">
-          Coupon
+        <Button color="success"> Coupon</Button>
         </Link>
       </li>
       <li className="nav-item">
         <Link to="/admin/category" className="nav-link">
-          Category
+        <Button color="success"> Category</Button>
         </Link>
       </li>
 
       <li className="nav-item">
         <Link to="/user/password" className="nav-link">
-          Password
+        <Button color="success"> Password</Button>
         </Link>
       </li>
     </ul>
