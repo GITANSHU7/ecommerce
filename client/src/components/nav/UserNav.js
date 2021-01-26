ada58898c02d8392b6132db6f4fcd5a02552696f
GITANSHU7 <gitanshugautam7@gmail.com>
2021-01-26 22:04:40 +0530

    button

--- client/src/components/nav/UserNav.js
+++ client/src/components/nav/UserNav.js
@@ -6,7 +6,7 @@ const UserNav = () => (
     <ul className="nav flex-column">
       <li className="nav-item">
         <Link to="/user/history" className="nav-link">
-          History
+        <button className="btn btn-info btn-raised pr-3 "> History</button>
         </Link>
       </li>
 
