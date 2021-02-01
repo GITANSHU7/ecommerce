866c0702d8a6d1f2ea942513edb9ef5c550b71dd
GITANSHU7 <gitanshugautam7@gmail.com>
2021-02-01 22:04:27 +0530

    updation

--- client/src/components/HomeCard/Cards.js
+++ client/src/components/HomeCard/Cards.js
@@ -5,43 +5,30 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Why Hypekar?</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              src='images/genuine.jpg'
+              text='Genuine products at reasonable price.'
               label='Adventure'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
+              src='images/oem.png'
+              text='Original OEM products only.'
               label='Luxury'
               path='/services'
             />
-          </ul>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+             <CardItem
+              src='images/stop.png'
+              text='One stop for all your needs.'
               label='Mystery'
               path='/services'
             />
-            <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
-              path='/products'
-            />
-            <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
-            />
           </ul>
+
         </div>
       </div>
     </div>
