41a2b70982bd4e34ffbdb308dfad3f178fdfa331
GITANSHU7 <gitanshugautam7@gmail.com>
2021-02-02 23:27:40 +0530

    updations

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,38 +1,19 @@
-import React, { Component } from 'react'
-import Jumbotron from '../components/cards/Jumbotron'
+import React from 'react'
+import Jumbotron from "../components/cards/Jumbotron";
 
-export default class About extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
-                 
-        }
 
-        this.handleEvent = this.handleEvent.bind(this)
-    }
+function About() {
+    
 
-    componentDidMount() {
-        
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) { if (prevState.name !== this.state.name) { this.handler() } }
-
-    componentWillUnmount() {
-        
-    }
-
-    // Prototype methods, Bind in Constructor (ES2015)
-    handleEvent() {}
-
-    // Class Properties (Stage 3 Proposal)
-    handler = () => { this.setState() }
-
-    render() {
-        return (
-            <div>
-                <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
-            </div>
-        )
-    }
+    return (
+        <>
+          <div className = "jumbotron text-danger h1 font-weight-bold text-center">
+          <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
+      
+              </div>  
+        </>
+    )
 }
+
+export default About
