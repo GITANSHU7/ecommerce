034da3314443a4aa76d09942e45fb4f0cc62830f
GITANSHU7 <gitanshugautam7@gmail.com>
2021-02-09 21:30:18 +0530

    updation

--- client/src/components/cards/ProductListItems.js
+++ client/src/components/cards/ProductListItems.js
@@ -2,14 +2,40 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 
-const ProductListItems = ({product}) => {
-    const {price} = product;
+const ProductListItems = ({product , match}) => {
+    const {price , brand,manufacturer, description,shipping,year,fuel,transmission,type ,  model} = product;
+   
+  
     return (
         <ul className = "list-group">
             <li className = "list-group-item">
                 Price
     <span className = "lable lable-default lable-pill pull-xs-right">
         {price}
+        </span>            
+            </li>
+            <li className = "list-group-item">
+                Manufacturer
+    <Link to = {`/brand/${brand.slug}`} className = "lable lable-default lable-pill pull-xs-right">
+        {brand.name}
+        </Link>            
+            </li>
+            <li className = "list-group-item">
+                type
+    <span className = "lable lable-default lable-pill pull-xs-right">
+        {type}
+        </span>            
+            </li>
+            <li className = "list-group-item">
+                Fuel
+    <span className = "lable lable-default lable-pill pull-xs-right">
+        {fuel}
+        </span>            
+            </li>
+            <li className = "list-group-item">
+                Transmission
+    <span className = "lable lable-default lable-pill pull-xs-right">
+        {transmission}
         </span>            
             </li>
         </ul>
