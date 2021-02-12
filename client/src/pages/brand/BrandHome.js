57f7631d67d65aaa409ac96e4f205daa4293d8b8
GITANSHU7 <gitanshugautam7@gmail.com>
2021-02-12 20:17:25 +0530

    product slug

--- client/src/pages/brand/BrandHome.js
+++ client/src/pages/brand/BrandHome.js
@@ -13,13 +13,40 @@ const CategoryHome = ({ match }) => {
 
   useEffect(() => {
     setLoading(true);
-    getBrand(slug).then((b) => {
-      console.log(JSON.stringify(b.data, null, 4));
-      setBrand(b.data);
+    getBrand(slug).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setBrand(res.data.brand);
+      setProducts(res.data.products);
+      setLoading(false);
     });
   }, []);
 
-  return <p>{slug}</p>;
+  return (
+    <div className="container-fluid">
+    <div className="row">
+      <div className="col">
+        {loading ? (
+          <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+            Loading...
+          </h4>
+        ) : (
+          <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+            {products.length} Products in "{brand.name}" brand
+          </h4>
+        )}
+      </div>
+    </div>
+
+    <div className="row">
+      {products.map((p) => (
+        <div className="col" key={p._id}>
+          <ProductCard product={p} />
+        </div>
+      ))}
+    </div>
+  </div>
+);
 };
+
 
 export default CategoryHome;
