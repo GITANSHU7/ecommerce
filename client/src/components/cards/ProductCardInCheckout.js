7b5f5fdba3636b754cd8617fd7dda676b2114697
GITANSHU7 <gitanshugautam7@gmail.com>
2021-02-18 14:50:21 +0530

    cart page setup

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,12 +1,19 @@
 import React from "react";
+import ModalImage from "react-modal-image";
 
 const ProductCardInCheckout = ({ p }) => {
   return (
     <tbody>
       <tr>
-        <td>Image</td>
+          <td>
+              <div style = {{width:"100px" , height:"100px"}} >
+              {p.images.length ? (<ModalImage small = {p.images[0].url} large = {p.images[0].url} />) : "" }
+         </div>
+          </td>
+
+        
         <td>{p.title}</td>
-        <td>${p.price}</td>
+        <td>₹{p.price}</td>
         <td>{p.manufacturer}</td>
         <td>{p.type}</td>
         <td>{p.count}</td>
