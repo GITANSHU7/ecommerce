91e98901765964fa209ec90df4e12c303494a131
GITANSHU7 <gitanshugautam7@gmail.com>
2021-02-20 23:05:12 +0530

    user 2

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -30,3 +30,35 @@ exports.userCart = async (req, res) => {
 
     products.push(object);
   }
+
+
+
+  // console.log('products', products)
+
+  let cartTotal = 0;
+  for (let i = 0; i < products.length; i++) {
+    cartTotal = cartTotal + products[i].price * products[i].count;
+  }
+
+  // console.log("cartTotal", cartTotal);
+
+  let newCart = await new Cart({
+    products,
+    cartTotal,
+    orderdBy: user._id,
+  }).save();
+
+  console.log("new cart ----> ", newCart);
+  res.json({ ok: true });
+};
+
+exports.getUserCart = async (req, res) => {
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  let cart = await Cart.findOne({ orderdBy: user._id })
+    .populate("products.product", "_id title price totalAfterDiscount")
+    .exec();
+
+  const { products, cartTotal, totalAfterDiscount } = cart;
+  res.json({ products, cartTotal, totalAfterDiscount });
+};
