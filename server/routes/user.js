baf44d8ee59e0fe4431e9a0ded80b5676f75603d
GITANSHU7 <gitanshugautam7@gmail.com>
2021-02-25 22:24:32 +0530

    pin routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,12 +5,12 @@ const router = express.Router();
 // middlewares
 const { authCheck } = require("../middlewares/auth");
 // controllers
-const { userCart , getUserCart , emptyCart , saveAddress} = require("../controllers/user");
+const { userCart , getUserCart , emptyCart , saveAddress , savePincode} = require("../controllers/user");
 
 router.post("/all/cart", authCheck, userCart); // save cart
 router.get("/all/cart", authCheck, getUserCart); // save cart
 router.put("/all/cart", authCheck, emptyCart); // delete cart
 router.post("/user/address" , authCheck , saveAddress);
-
+router.post("/user/pincode", authCheck , savePincode);
 
 module.exports = router;
