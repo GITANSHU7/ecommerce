01e493741e1b53d604c9e9972071173c5a1135ce
GITANSHU7 <gitanshugautam7@gmail.com>
2021-02-26 21:19:29 +0530

    coupon front setup

--- client/src/App.js
+++ client/src/App.js
@@ -42,6 +42,7 @@ import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
 import SideDrawer from "./components/drawer/SideDrawer";
 import Checkout from "./pages/Checkout";
+import CreateCouponPage from "./pages/coupon/CreateCouponPage";
 
 
 
@@ -117,6 +118,8 @@ const App = () => {
         <Route exact path="/shop" component={Shop} />
         <Route exact path="/cart" component={Cart} />
         <Route exact path="/checkout" component={Checkout} />
+        <AdminRoute exact path="/admin/coupon" component={CreateCouponPage} />
+      
 
     </Switch>
     
