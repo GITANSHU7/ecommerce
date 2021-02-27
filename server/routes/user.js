d21bb3a6f89906e33e5ff72f2b651f7c02a39935
GITANSHU7 <gitanshugautam7@gmail.com>
2021-02-27 22:57:52 +0530

    user route

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,12 +5,16 @@ const router = express.Router();
 // middlewares
 const { authCheck } = require("../middlewares/auth");
 // controllers
-const { userCart , getUserCart , emptyCart , saveAddress , savePincode} = require("../controllers/user");
+const { userCart , getUserCart , emptyCart , saveAddress , savePincode, applyCouponToUserCart} = require("../controllers/user");
 
 router.post("/all/cart", authCheck, userCart); // save cart
 router.get("/all/cart", authCheck, getUserCart); // save cart
 router.put("/all/cart", authCheck, emptyCart); // delete cart
 router.post("/user/address" , authCheck , saveAddress);
 router.post("/user/pincode", authCheck , savePincode);
+
+//coupon 
+router.post("/user/cart/coupon" , authCheck,applyCouponToUserCart);
+
 
 module.exports = router;
