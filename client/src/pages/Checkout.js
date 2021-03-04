f23762cf8be5b2cecf071f5ad5efe044f9d4d710
GITANSHU7 <gitanshugautam7@gmail.com>
2021-03-04 20:17:17 +0530

    razor pay inte

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import { getUserCart, emptyUserCart , saveUserAddress , saveUserPincode , applyCoupon  , saveUserLocality , saveUserContact, saveUserName} from "../functions/user";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.bubble.css";
+import axios from "axios";
+import Logo from "../logo";
 
 const Checkout = ({history}) => {
   const [products, setProducts] = useState([]);
@@ -32,6 +32,9 @@ const Checkout = ({history}) => {
       setTotal(res.data.cartTotal);
     });
   }, []);
+
+
+  
 
   const emptyCart = () => {
     // remove from local storage
@@ -244,6 +247,78 @@ const Checkout = ({history}) => {
           }
 
 
+          function loadScript(src) {
+            return new Promise((resolve) => {
+                const script = document.createElement("script");
+                script.src = src;
+                script.onload = () => {
+                    resolve(true);
+                };
+                script.onerror = () => {
+                    resolve(false);
+                };
+                document.body.appendChild(script);
+            });
+    }
+
+    async function displayRazorpay() {
+      const res = await loadScript(
+          "https://checkout.razorpay.com/v1/checkout.js"
+      );
+
+      if (!res) {
+          alert("Razorpay SDK failed to load. Are you online?");
+          return;
+      }
+
+      // creating a new order
+      const result = await axios.post("http://localhost:5000/payment/orders");
+
+      if (!result) {
+          alert("Server error. Are you online?");
+          return;
+      }
+
+      // Getting the order details back
+      const { amount, id: order_id, currency } = result.data;
+
+      const options = {
+          key: "rzp_test_r6FiJfddJh76SI", // Enter the Key ID generated from the Dashboard
+          amount: amount.toString(),
+          currency: currency,
+          name: "Soumya Corp.",
+          description: "Test Transaction",
+          image: { Logo },
+          order_id: order_id,
+          handler: async function (response) {
+              const data = {
+                  orderCreationId: order_id,
+                  razorpayPaymentId: response.razorpay_payment_id,
+                  razorpayOrderId: response.razorpay_order_id,
+                  razorpaySignature: response.razorpay_signature,
+              };
+
+              const result = await axios.post("http://localhost:8000/payment/success", data);
+
+              alert(result.data.msg);
+          },
+          prefill: {
+              name: "Soumya Dey",
+              email: "SoumyaDey@example.com",
+              contact: "9999999999",
+          },
+          notes: {
+              address: "Soumya Dey Corporate Office",
+          },
+          theme: {
+              color: "#61dafb",
+          },
+      };
+
+      const paymentObject = new window.Razorpay(options);
+      paymentObject.open();
+}
+
   return (
     <div className="row">
       <div className="col-md-6">
@@ -276,7 +351,7 @@ const Checkout = ({history}) => {
           <div className="col-md-6">
             <button className="btn btn-primary"   
             disabled = {!address.length || !pincode.length || !products.length ||!name.length ||!locality.length ||!contact.length} 
-            onClick = {() => history.push("/payment")}
+            onClick = {() => {displayRazorpay}}
             >
               Place Order</button>
           </div>
