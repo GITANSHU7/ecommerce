d94492134e4a4e92f5a3957ef84b04d546e5ae67
GITANSHU7 <gitanshugautam7@gmail.com>
2021-03-05 20:27:54 +0530

    stirpe payment

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -7,19 +7,30 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
 exports.createPaymentIntent = async (req, res) => {
   // later apply coupon
   // later calculate price
-
+const {couponApplied} = req.body
     // 1 find user
     const user = await User.findOne({ email: req.user.email }).exec();
     // 2 get user cart total
-    const { cartTotal } = await Cart.findOne({ orderdBy: user._id }).exec();
-  
-  
+    const { cartTotal  , totalAfterDiscount} = await Cart.findOne({ orderdBy: user._id }).exec();
+
+      let finalAmount = 0 ;
+
+      if(couponApplied && totalAfterDiscount) {
+        finalAmount = Math.round(totalAfterDiscount * 100);
+      } else {
+        finalAmount = Math.round(cartTotal * 100)
+      }
+
+    
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: cartTotal * 100,
+    amount: finalAmount,
     currency: "inr",
   });
 
   res.send({
     clientSecret: paymentIntent.client_secret,
+    cartTotal,
+    totalAfterDiscount,
+    payable : finalAmount,
   });
 };
