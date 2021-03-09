8df1331622a00eedf5dfe6288be0133a315ed752
GITANSHU7 <gitanshugautam7@gmail.com>
2021-03-09 21:02:53 +0530

    OUT OF STOCK

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -70,9 +70,10 @@ const [tooltip, setTooltip] = useState('Click to add')
           <EyeOutlined className="text-warning" /> <br /> View Product
         </Link>,
         <Tooltip title={tooltip}>
-        <a onClick={handleAddToCart}>
-          <ShoppingCartOutlined className="text-danger" /> <br /> Add to Cart
-        </a> </Tooltip> ,
+        <a onClick={handleAddToCart} disabled = {product.quantity < 1} >
+          <ShoppingCartOutlined className="text-danger" /> <br /> 
+        {product.quantity < 1 ? "OUT OF STOCK" : "ADD TO CART"}
+      </a> </Tooltip> ,
       ]}
     >
       <Meta
