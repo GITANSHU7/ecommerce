ea90ae46fed33905eb44959266ea36ca60efbd23
GITANSHU7 <gitanshugautam7@gmail.com>
2021-03-13 10:36:39 +0530

    function'

--- client/src/functions/user.js
+++ client/src/functions/user.js
@@ -102,3 +102,19 @@ await axios.get(`${process.env.REACT_APP_API}/user/orders`, {
     authtoken,
   },
 });
+
+export const removeWishlist = async (productId , authtoken) =>
+await axios.put(`${process.env.REACT_APP_API}/user/wishlist`, {productId} , {
+  headers: {
+    authtoken,
+  },
+});
+
+
+export const addToWishlist = async (productId , authtoken) =>
+await axios.post(`${process.env.REACT_APP_API}/user/wishlist`, {productId} , {
+  headers: {
+    authtoken,
+  }, 
+});
+
