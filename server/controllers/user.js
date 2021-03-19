4098031d34abdd0753a7a40bdc372ffd60b78390
GITANSHU7 <gitanshugautam7@gmail.com>
2021-03-19 22:36:47 +0530

    date setup 2'

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -264,7 +264,7 @@ exports.createCashOrder = async (req, res) => {
       amount: finalAmount,
       currency: "inr",
       status: "Cash On Delivery",
-      created: new Date(),
+      created: today.toDateString(),
       payment_method_types: ["cash"],
     },
     orderdBy: user._id,
