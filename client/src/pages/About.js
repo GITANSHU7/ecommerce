76f88bd59b41cab2078d03f94fe6325a9439430a
GITANSHU7 <gitanshugautam7@gmail.com>
2021-04-02 22:31:59 +0530

    about page setup 1

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,4 +1,28 @@
-import React from 'react';
+import React from 'react'
+import Jumbotron from "../components/cards/Jumbotron";
+import { MoreAboutSection } from '../components/HomeCard/moreAboutSection';
+import { Footer } from '../components/Working/footer';
+
+
+
+function About() {
+    
+
+    return (
+        <>
+          <div className = "jumbotron text-danger h1 font-weight-bold text-center">
+          <Jumbotron text={["Maintainance Kit", "Accessories", "OEM Parts"]} />
+      
+              </div>  
+              <MoreAboutSection />
+              <Footer />
+        </>
+    )
+}
+
+export default About ;
+
+{/*import React from 'react';
 
 import { Row, Col } from 'antd';
 
@@ -72,27 +96,5 @@ function AppAbout() {
   );
 }
 
-export default AppAbout;
-{/* import React from 'react'
-import Jumbotron from "../components/cards/Jumbotron";
-import { MoreAboutSection } from '../components/HomeCard/moreAboutSection';
-import { Footer } from '../components/Working/footer';
-
-
-
-function About() {
-    
-
-    return (
-        <>
-          <div className = "jumbotron text-danger h1 font-weight-bold text-center">
-          <Jumbotron text={["Maintainance Kit", "Accessories", "OEM Parts"]} />
-      
-              </div>  
-              <MoreAboutSection />
-              <Footer />
-        </>
-    )
-}
-
-export default About */}
+export default AppAbout; */}
+ 
